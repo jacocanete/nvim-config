a16ec18036e9fd3470ccbbaf3803b123b214ccce
jcaneteDI <jcanete@digitalimpulse.com>
2024-09-10 14:22:21 +0800

    Add new keymap and update wrong keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,9 @@
 
 local keymap = vim.keymap -- for conciseness
 
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>+", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 keymap.set("n", "<leader>nh", "<:nohl<CR>", { desc = "Clear search highlights" })
